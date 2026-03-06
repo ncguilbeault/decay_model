33656c8c4d90c4678d739e0076b1b944d56d4397
ncguilbeault <ncguilbeault@gmail.com>
2026-03-06 01:24:51 +0000

    Updated code with formatting and added model export function

--- src/decay_model/modules/spike_localization.py
+++ src/decay_model/modules/spike_localization.py
@@ -2,20 +2,24 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-EPSILON = 1e-6
-
 
 class SpikeLocalizationEncoder(nn.Module):
 
     def __init__(
-        self, waveforms_dim: int, capacity1: int = 500, capacity2: int = 200, dropout1: float = 0, dropout2: float = 0, device: str = "cuda"
+        self,
+        waveforms_dim: int,
+        capacity1: int = 500,
+        capacity2: int = 200,
+        dropout1: float = 0.0,
+        dropout2: float = 0.0,
+        epsilon: float = 1e-6,
+        device: str = "cuda",
     ):
         super(SpikeLocalizationEncoder, self).__init__()
 
         self.capacity1 = capacity1
         self.capacity2 = capacity2
-        self.dropout1 = dropout1
-        self.dropout2 = dropout2
+        self.epsilon = epsilon
 
         self.fc1 = nn.Linear(waveforms_dim, self.capacity1, device=device)
         self.fc2 = nn.Linear(self.capacity1, self.capacity2, device=device)
@@ -25,25 +29,36 @@ class SpikeLocalizationEncoder(nn.Module):
         self.batchnorm1 = nn.BatchNorm1d(self.capacity1, device=device)
         self.batchnorm2 = nn.BatchNorm1d(self.capacity2, device=device)
 
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+
+        self.dropout1 = nn.Dropout(dropout1)
+        self.dropout2 = nn.Dropout(dropout2)
+
+        self.softplus = nn.Softplus()
+
         self.apply(_weight_init)
         self.to(device)
 
     def forward(self, waveforms: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        x = self.batchnorm1(self.fc1(torch.flatten(waveforms, start_dim=1)))
-        x = F.relu(x)
-        x = F.dropout(x, p=self.dropout1)
-        x = self.batchnorm2(self.fc2(x))
-        x = F.relu(x)
-        x = F.dropout(x, p=self.dropout2)
+        x = torch.flatten(waveforms, start_dim=1)
+        x = self.fc1(x)
+        x = self.batchnorm1(x)
+        x = self.relu1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.batchnorm2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
         xyz_mu = self.fc_mean(x)
-        xyz_var = F.softplus(self.fc_var(x)) + EPSILON
+        x = self.fc_var(x)
+        xyz_var = self.softplus(x) + self.epsilon
         return xyz_mu, xyz_var
+
 
 class SpikeLocalizationDecoder(nn.Module):
 
-    def __init__(
-        self, exps: torch.Tensor, ch_locs: torch.Tensor, device: str = "cuda"
-    ):
+    def __init__(self, exps: torch.Tensor, ch_locs: torch.Tensor, device: str = "cuda"):
         super(SpikeLocalizationDecoder, self).__init__()
 
         self.ch_locs = ch_locs
@@ -51,53 +66,94 @@ class SpikeLocalizationDecoder(nn.Module):
 
         self.to(device)
 
-    def forward(self, sampled_n_loc: torch.Tensor, exp_ids: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, sampled_n_loc: torch.Tensor, exp_ids: torch.Tensor
+    ) -> torch.Tensor:
         n_loc = sampled_n_loc.view(sampled_n_loc.shape[0], 1, sampled_n_loc.shape[1])
         subtract = (n_loc - self.ch_locs[exp_ids]) ** 2
         summed = torch.sum(subtract, dim=2)
         distances = torch.sqrt(summed)
-        recon_amps = -torch.exp(distances * self.exps[exp_ids, 1].unsqueeze(-1) + self.exps[exp_ids, 0].unsqueeze(-1))
+        recon_amps = -torch.exp(
+            distances * self.exps[exp_ids, 1].unsqueeze(-1)
+            + self.exps[exp_ids, 0].unsqueeze(-1)
+        )
         return recon_amps
+
 
 class SpikeLocalizationVAE(nn.Module):
 
     def __init__(
-        self, waveforms_dim: int, exps: torch.Tensor, ch_locs: torch.Tensor, capacity1: int = 500, capacity2: int = 200, dropout1: float = 0, dropout2: float = 0, device: str = "cuda"
+        self,
+        waveforms_dim: int,
+        exps: torch.Tensor,
+        ch_locs: torch.Tensor,
+        capacity1: int = 500,
+        capacity2: int = 200,
+        dropout1: float = 0.0,
+        dropout2: float = 0.0,
+        epsilon: float = 1e-6,
+        device: str = "cuda",
     ):
         super(SpikeLocalizationVAE, self).__init__()
 
         self.encoder = SpikeLocalizationEncoder(
-            waveforms_dim, capacity1, capacity2, dropout1, dropout2, device
+            waveforms_dim, capacity1, capacity2, dropout1, dropout2, epsilon, device
         )
         self.decoder = SpikeLocalizationDecoder(exps, ch_locs, device)
 
-    def reparameterize_normal(self, mu: torch.Tensor, var: torch.Tensor) -> torch.Tensor:
+    def reparameterize_normal(
+        self, mu: torch.Tensor, var: torch.Tensor
+    ) -> torch.Tensor:
         std = var.sqrt()
         eps = torch.randn_like(std)
         return eps * std + mu
 
-    def forward(self, waveforms: torch.Tensor, exp_ids: torch.Tensor, training: bool) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def forward(
+        self, waveforms: torch.Tensor, exp_ids: torch.Tensor, training: bool
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         xyz_mu, xyz_var = self.encoder(waveforms)
         if training:
             xyz_mu = self.reparameterize_normal(xyz_mu, xyz_var)
         recon_amps = self.decoder(xyz_mu, exp_ids)
         return recon_amps, xyz_mu, xyz_var
-    
+
+
 class SpikeLocalizationLossFunc(nn.Module):
-    
+
     def __init__(self, prior_var: float, device="cuda"):
         super(SpikeLocalizationLossFunc, self).__init__()
-        self.prior_var = torch.cat((torch.tensor(prior_var**2).unsqueeze(0), torch.tensor(prior_var**2).unsqueeze(0), torch.tensor(prior_var**2).unsqueeze(0))).view(1, -1).to(device)
+        self.prior_var = torch.ones(3, device=device) * (prior_var**2)
         self.to(device)
 
-    def kl_divergence_normal(self, mu_q: torch.Tensor, var_q: torch.Tensor, mu_p: torch.Tensor, var_p: torch.Tensor) -> torch.Tensor:
-        return torch.sum(0.5 * (torch.log(var_p) - torch.log(var_q)) + torch.div(var_q + (mu_q - mu_p) ** 2, 2 * var_p - 0.5))
+    def kl_divergence_normal(
+        self,
+        mu_q: torch.Tensor,
+        var_q: torch.Tensor,
+        mu_p: torch.Tensor,
+        var_p: torch.Tensor,
+    ) -> torch.Tensor:
+        return torch.sum(
+            0.5 * (torch.log(var_p) - torch.log(var_q))
+            + torch.div(var_q + (mu_q - mu_p) ** 2, 2 * var_p)
+            - 0.5
+        )
 
-    def forward(self, recon_amps: torch.Tensor, amps: torch.Tensor, xyz_mu: torch.Tensor, xyz_var: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self,
+        recon_amps: torch.Tensor,
+        amps: torch.Tensor,
+        xyz_mu: torch.Tensor,
+        xyz_var: torch.Tensor,
+    ) -> torch.Tensor:
         batch_size = recon_amps.shape[0]
         xyz_muq = torch.zeros_like(xyz_mu)
         xyz_varq = torch.ones_like(xyz_var) * self.prior_var
-        loss = (torch.sum(F.mse_loss(recon_amps, amps[:, :, 0], reduction="none") * amps[:, :, 1]) + self.kl_divergence_normal(xyz_mu, xyz_var, xyz_muq, xyz_varq)) / batch_size
+        loss = (
+            torch.sum(
+                F.mse_loss(recon_amps, amps[:, :, 0], reduction="none") * amps[:, :, 1]
+            )
+            + self.kl_divergence_normal(xyz_mu, xyz_var, xyz_muq, xyz_varq)
+        ) / batch_size
         return loss
 
 
--- src/decay_model/utilities/__init__.py
+++ src/decay_model/utilities/__init__.py
@@ -1,14 +1,24 @@
 import xml.etree.ElementTree as ET
 import torch
 
-def load_channel_positions_from_xml(xml_path: str, subtract_mean: bool = True, use_3d: bool = True, device: torch.device = "cpu") -> torch.Tensor:
+
+def load_channel_positions_from_xml(
+    xml_path: str,
+    subtract_mean: bool = True,
+    use_3d: bool = True,
+    device: torch.device = "cpu",
+) -> torch.Tensor:
     tree = ET.parse(xml_path)
     root = tree.getroot()
     xpos = root[3][0][-1][-1][-2].attrib
     ypos = root[3][0][-1][-1][-1].attrib
 
-    assert len(xpos) == len(ypos), "The number of x and y channel positions must be the same"
-    assert all(key in ypos for key in xpos) and all(key in xpos for key in ypos), "The channel keys in x and y positions must match"
+    assert len(xpos) == len(
+        ypos
+    ), "The number of x and y channel positions must be the same"
+    assert all(key in ypos for key in xpos) and all(
+        key in xpos for key in ypos
+    ), "The channel keys in x and y positions must match"
 
     channel_ids = []
     channel_keys = xpos.keys()
@@ -28,15 +38,20 @@ def load_channel_positions_from_xml(xml_path: str, subtract_mean: bool = True, u
             pos = torch.tensor([x, y], device=device)
         channel_positions[channel_id] = pos
 
-    idx_to_keep = torch.where(torch.any(channel_positions!=0, dim=1))[0]
+    idx_to_keep = torch.where(torch.any(channel_positions != 0, dim=1))[0]
     channel_positions = channel_positions[idx_to_keep]
 
     if subtract_mean:
         channel_positions -= torch.mean(channel_positions, dim=0)
-    
+
     return channel_positions
 
-def pad_channel_positions(channel_positions: torch.Tensor, x_spacing: float | None = None, y_spacing: float | None = None) -> torch.Tensor:
+
+def pad_channel_positions(
+    channel_positions: torch.Tensor,
+    x_spacing: float | None = None,
+    y_spacing: float | None = None,
+) -> torch.Tensor:
 
     if channel_positions.shape[1] == 2:
         x_coords = torch.unique(channel_positions[:, 0])
@@ -44,20 +59,32 @@ def pad_channel_positions(channel_positions: torch.Tensor, x_spacing: float | No
     else:
         x_coords = torch.unique(channel_positions[:, 1])
         y_coords = torch.unique(channel_positions[:, 2])
-    
-    assert x_spacing is not None or x_coords.shape[0] > 1, "If x_spacing is not provided, there must be more than one unique x coordinate to infer spacing"
 
-    assert y_spacing is not None or y_coords.shape[0] > 1, "If y_spacing is not provided, there must be more than one unique y coordinate to infer spacing"
+    assert (
+        x_spacing is not None or x_coords.shape[0] > 1
+    ), "If x_spacing is not provided, there must be more than one unique x coordinate to infer spacing"
+
+    assert (
+        y_spacing is not None or y_coords.shape[0] > 1
+    ), "If y_spacing is not provided, there must be more than one unique y coordinate to infer spacing"
 
     if x_spacing is None:
         x_spacing = x_coords[-1] - x_coords[0] + (x_coords[1] - x_coords[0])
-    
+
     if y_spacing is None:
         y_spacing = y_coords[-1] - y_coords[0] + (y_coords[1] - y_coords[0])
 
-    offsets = torch.tensor([[0, i * x_spacing, j * y_spacing] for i in range(-1, 2) for j in range(-1, 2) if (i, j) != (0, 0)], device=channel_positions.device)
+    offsets = torch.tensor(
+        [
+            [0, i * x_spacing, j * y_spacing]
+            for i in range(-1, 2)
+            for j in range(-1, 2)
+            if (i, j) != (0, 0)
+        ],
+        device=channel_positions.device,
+    )
 
     shifted_positions = channel_positions.unsqueeze(1) + offsets.unsqueeze(0)
-    return torch.vstack((channel_positions, shifted_positions.view(-1, channel_positions.shape[1])))
-
-    
+    return torch.vstack(
+        (channel_positions, shifted_positions.view(-1, channel_positions.shape[1]))
+    )
